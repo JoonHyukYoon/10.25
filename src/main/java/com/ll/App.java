571231db4a812035d6df46d768967fb54e68e100
JoonHyukYoon <crayngnuk1@naver.com>
2023-10-25 21:00:35 +0900

    App 클래스를 별도의 파일로

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,9 @@
+package com.ll;
+
+public class App {
+    class App {
+        void run(){
+            System.out.println("프로그램 실행");
+        }
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,8 +5,4 @@ public class Main {
         new App().run();
     }
 }
-class App {
-    void run(){
-        System.out.println("프로그램 실행");
-    }
-}
+
