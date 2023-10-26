87ae84675d2b76643c54162ed2df85e270a087d6
JoonHyukYoon <crayngnuk1@naver.com>
2023-10-26 16:14:09 +0900

    1 단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,27 +5,11 @@ import java.util.Scanner;
 public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
+        System.out.print("등록)");
 
-        while (true) {
-            System.out.println("명령)");
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
 
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.nextLine();
-
-
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                scanner.nextLine();
-                System.out.print("작가 : ");
-                scanner.nextLine();
-            }
-
-            System.out.printf("%s\n", cmd);
-
-
-        }
+        System.out.printf("%s\n", cmd);
     }
-
 }
