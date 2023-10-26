9916a6e997dcf7da44c4fe49aeb980e2f90268b7
JoonHyukYoon <crayngnuk1@naver.com>
2023-10-26 14:43:53 +0900

    2단ã계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,9 +1,31 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
-    class App {
-        void run(){
-            System.out.println("프로그램 실행");
+    void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.println("명령)");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                scanner.nextLine();
+                System.out.print("작가 : ");
+                scanner.nextLine();
+            }
+
+            System.out.printf("%s\n", cmd);
+
+
         }
     }
+
 }
