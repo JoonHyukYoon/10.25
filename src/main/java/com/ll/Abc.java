8a33eaf85f5dfbb09226dbffcf6ae0169bbbd82b
JoonHyukYoon <crayngnuk1@naver.com>
2023-10-27 17:14:07 +0900

    5단계 파일추가

---
+++ src/main/java/com/ll/Abc.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Abc {
+    int id;
+    String content;
+    String authorName;
+
+    public Abc(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,11 +5,35 @@ import java.util.Scanner;
 public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("등록)");
+        int lastAbcId = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+        while (true) {
 
-        System.out.printf("%s\n", cmd);
+            System.out.print("명령)");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료"))
+                break;
+
+            else if (cmd.equals("등록")) {
+
+                System.out.print("명언 : ");
+                scanner.nextLine();
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                lastAbcId++;
+                int id = lastAbcId;
+                Abc abc = new Abc(id, content, authorName);
+
+
+
+                System.out.printf("%d 번 명언이 등록되었습니다.\n", lastAbcId);
+            }
+        }
+
     }
 }
